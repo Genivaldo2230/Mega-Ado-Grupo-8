757d442922a5ed1d665e0a4b45f19a08aafdd3c7
elvis.emandrade <elvisederson@hotmail.com>
2022-05-09 21:11:28 -0300

    Alteração Final

--- Grupo_8_Mega_Ado/src/main/java/com/senac/sp/log/algorit/grupo_8_mega_ado/ViagemGuaruja2.java
+++ Grupo_8_Mega_Ado/src/main/java/com/senac/sp/log/algorit/grupo_8_mega_ado/ViagemGuaruja2.java
@@ -12,15 +12,15 @@ public class ViagemGuaruja2 {
         disponibilidade = sc.nextInt();
 
         if (disponibilidade == 1) {
-            System.out.println("Tenho orçamento para esta viagem? Digite 1 para SIM e 2 para NÂO.");
+            System.out.println("Tenho orçamento para esta viagem? \nDigite o valor do seu orçamento.");
             orcamento = sc.nextInt();
-            if (orcamento == 1) {
-                System.out.println("Como estão as condições climaticas? Digite 1 para SIM e 2 para NÂO.");
+            if (orcamento >= 2000) {
+                System.out.println("Como estão as condições climaticas? Digite a temperatura para o final de semana.");
                 clima = sc.nextInt();
             } else {
             }
         }
-        if (clima == 1) {
+        if (clima >= 25) {
             System.out.println("A viagem será de Carro ou de Onibus? Digite 1 para CARRO PRÓPRIO e 2 para CARRO ALUGADO.");
             transporte = sc.nextInt();
             if (transporte == 1) {
